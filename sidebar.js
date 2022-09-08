58dfcad168cd68cb2636571366e43b19251db44b
LYD <1841720844@qq.com>
2022-09-08 15:45:58 +0800

    网页

--- sidebar.js
+++ sidebar.js
@@ -1,73 +1,65 @@
 $(function () {
    // 侧边栏弹出
+   var count = 1;
    $('button').click(function () {
-      var left = $('#wrapper')[0].offsetLeft;
-      if (left == 0) {
-         $('#wrapper').offset({'left': 220});
-         $('#wrapper').css({'transition': "all .6s"});
+      count++;
+      if (count % 2 === 0) {
+         $('.sidebar').css({ "left": 0, "transition": "all .6s" });
+         $(this).css("marginLeft", 230);
          $(this).css('transform', 'rotate(450deg)');
          $('.headSculpture img').addClass('img');
          $('.headSculpture p').addClass('opacity');
          setTimeout(function () {
             $('.option ul>li').addClass('li');
-         }, 600)
+         }, 600);
       } else {
-         $('#wrapper').offset({
-            'left': 0
-         });
+         $('.sidebar').css({ "left": -230 });
+         $(this).css("marginLeft", 10);
          $(this).css('transform', 'rotate(0deg)');
          setTimeout(function () {
             $('.headSculpture img').removeClass('img');
             $('.headSculpture p').removeClass('opacity');
             $('.option ul>li').removeClass('li');
-         }, 300)
+         }, 300);
       }
-   })
+   });
 
    // 鼠标触碰改变img样式
    var SRC = [{
       src: 'images/home_1.png'
    },
    {
-      src: 'images/works_1.png'
-   },
-   {
       src: 'images/html_1.png'
-   },
-   {
-      src: 'images/webPage_1.png'
    },
    {
       src: 'images/special_1.png'
    },
    {
-      src: 'images/HTML5_1.png'
-   },
-   {
       src: 'images/home.png'
-   },
-   {
-      src: 'images/works.png'
    },
    {
       src: 'images/html.png'
    },
    {
-      src: 'images/webPage.png'
-   },
-   {
       src: 'images/special.png'
    },
-   {
-      src: 'images/HTML5.png'
-   },
-   ]
+   ];
    $('.option ul>li').mouseenter(function () {
       var index = $(this).index();
       $(this).children('img').attr('src', SRC[index].src);
-   })
+   });
    $('.option ul>li').mouseleave(function () {
       var index = $(this).index();
-      $(this).children('img').attr('src', SRC[index + 6].src);
-   })
-})
+      $(this).children('img').attr('src', SRC[index + 3].src);
+   });
+
+   // 点击分类显示对应内容
+   $('.list li').click(function () {
+      // $(this).addClass('active').siblings().removeClass("active")
+      $('.banner').css({
+         top: -753.6 * $(this).index(),
+         transition: "all .3s"
+      });
+   });
+
+});
